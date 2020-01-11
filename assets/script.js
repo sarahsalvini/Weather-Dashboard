4e8f5128b48f5c32d52957863f0a3be77a5cdc22
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-01-11 15:40:07 -0600

    was able to get user input to print out buttons

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,33 @@
 $(document).ready(function(){
 
- apiKey = "ef82854d1d438966fb160a6d8e7f319b";
+    var apiKey = "ef82854d1d438966fb160a6d8e7f319b";
 
-// pseudo code:
-// 1. be able to search for a city
-// 2. have the search save to the list below the search
-//      in a button where you can reclick it
-// 3. when you search for a city have the 5 day forecast 
-//      popup with 
-// 4. show current and future forecast 
-// 5. must include city name, the date, an icon,
-//      temp, and humidity
-// 6. color cordinate the UV index
-
-let weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=ef82854d1d438966fb160a6d8e7f319b";
-let city = "";
-
-$("#history").on("click", function(){
-    currentForecast();
-    fiveDay();
-})
-
-function currentForecast() {
+    function searchWeather(city) {
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
     
-}
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        console.log(response)
+        var cityName = $("<button>").html(response.name);
+            $("#city-div").append(cityName);
+    })
+    }
+    
+    $("#select-city").on("click", function(event) {
+        event.preventDefault();
+        var city = $("#city-input").val().trim();
+    
+        searchWeather(city);
+    })
 
-function fiveDay() {
 
-}
 
-})
+
+
+
+
+
+
+});
