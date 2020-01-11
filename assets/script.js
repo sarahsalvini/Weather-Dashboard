5f0076169450be637e304d747cfe75335a6222e7
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-01-11 16:30:42 -0600

    printed out current weather is body

--- assets/script.js
+++ assets/script.js
@@ -10,15 +10,27 @@ $(document).ready(function(){
         method: "GET"
     }).done(function(response){
         console.log(response)
-        var cityName = $("<button>").html(response.name);
-            $("#city-div").append(cityName);
+        var history = $("<button>").html(response.name);
+            $("#city-div").append(history);
+
+        var cityName = $("#cityTitle").text(response.name);
+            $("#currentWeather").append(cityName);
+        var cityTemp = $("#temperature").text("Temperature: " + response.main.temp);
+            $("#currentWeather").append(cityTemp);
+        var cityHum = $("#humidity").text("Humidity: " + response.main.humidity + " %");
+            $("#currentWeather").append(cityHum);
+        var cityWind = $("#windSpeed").text("Wind Speed: " + response.wind.speed + " MPH");
+            $("#currentWeather").append(cityWind);
+        var cityUV = $("#UV").text("UV Index: ");
+            $("#currentWeather").append(cityUV);
     })
     }
     
     $("#select-city").on("click", function(event) {
         event.preventDefault();
         var city = $("#city-input").val().trim();
-    
+
+
         searchWeather(city);
     })
 
@@ -26,8 +38,17 @@ $(document).ready(function(){
 
 
 
-
-
+    // name
+    // temp: 
+    //     main.temp
+    
+    // humidity: 
+    //     main.humidity
+    
+    // wind speed:
+    //     wind.speed
+    
+    // UV Index:
 
 
 });
